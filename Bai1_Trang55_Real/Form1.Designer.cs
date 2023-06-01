4264a4b3b4fc2a4df8e93fdea4398cc46ee94632
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-01 23:10:12 +0700

    DungNguyen

---
+++ Bai1_Trang55_Real/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Bai1_Trang55_Real
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtThang = new System.Windows.Forms.TextBox();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.btnKetQua = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập tháng:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập năm:";
+            // 
+            // txtThang
+            // 
+            this.txtThang.Location = new System.Drawing.Point(210, 51);
+            this.txtThang.Name = "txtThang";
+            this.txtThang.Size = new System.Drawing.Size(551, 22);
+            this.txtThang.TabIndex = 2;
+            this.txtThang.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtThang_KeyPress);
+            // 
+            // txtNam
+            // 
+            this.txtNam.Location = new System.Drawing.Point(210, 112);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(551, 22);
+            this.txtNam.TabIndex = 3;
+            this.txtNam.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtThang_KeyPress);
+            // 
+            // btnKetQua
+            // 
+            this.btnKetQua.Location = new System.Drawing.Point(60, 265);
+            this.btnKetQua.Name = "btnKetQua";
+            this.btnKetQua.Size = new System.Drawing.Size(203, 43);
+            this.btnKetQua.TabIndex = 4;
+            this.btnKetQua.Text = "&Kết Quả";
+            this.btnKetQua.UseVisualStyleBackColor = true;
+            this.btnKetQua.Click += new System.EventHandler(this.btnKetQua_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(306, 265);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(203, 43);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(558, 265);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(203, 43);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "&Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AllowDrop = true;
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.Location = new System.Drawing.Point(63, 180);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(0, 25);
+            this.lblKetQua.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 336);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnKetQua);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.txtThang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bài 1 Trang 55";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtThang;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.Button btnKetQua;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ Bai1_Trang55_Real/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_Trang55_Real
+{
+    public partial class Form1 : Form
+    {
+        int thang, nam;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtThang_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Xác thực rằng phím vừa nhấn không phải CTRL hoặc không phải dạng số
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            txtThang.MaxLength = 2;
+            txtNam.MaxLength = 4;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblKetQua.Text = string.Empty;
+            txtNam.Text = string.Empty;
+            txtThang.Text = string.Empty;
+            txtThang.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // user clicked yes
+                this.Close();
+            }
+        }
+
+        private void btnKetQua_Click(object sender, EventArgs e)
+        {
+            thang = Convert.ToInt32(txtThang.Text);
+            if(thang <1 || thang >12)
+            {
+                MessageBox.Show("Nhập tháng từ 1 -> 12 !");
+                txtThang.Text = String.Empty;
+                txtThang.Focus();
+                return;
+            }
+
+            nam = Convert.ToInt32(txtNam.Text);
+            if (nam <1000 || nam>9999)
+            {
+                MessageBox.Show("Nhập năm trong [1000,9999]");
+                txtNam.Text = String.Empty;
+                txtNam.Focus();
+                return;
+            }
+
+            lblKetQua.Text = "Tháng " + thang + " năm " + nam + " có " + DateTime.DaysInMonth(nam,thang);
+        }
+    }
+}
